49917e08fa08c29a8c37210526ac2a6bb7523778
DhiMalo <aherisan@gmail.com>
2015-10-30 17:20:57 -0700

    Added notes for planned implementation.

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,11 @@
 var mysql = require('mysql');
 
+export.connectToDatabase = function (){
+
+}
+
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-
+// TO DO : Look up the docs on mysqul node package and add a function to do: mysql -u root
